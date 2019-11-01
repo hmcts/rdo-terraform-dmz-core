35dec278e4a1595649c1d6befb7744db15e69ac0
Piyush Kumar <piyush.kumar@hmcts.net>
2019-11-01 13:12:06 +0000

    update config for ddos and azure firewall

--- 12-subnets.tf
+++ 12-subnets.tf
@@ -48,6 +48,13 @@ resource "azurerm_subnet" "subnet-dmz-palo-private" {
 
 }
 
+resource "azurerm_subnet" "subnet-dmz-azure-fw" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = "${azurerm_resource_group.rg_dmz.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet_dmz.name}"
+  address_prefix       = "${var.subnet-dmz-azure-fw-prefix}"
+}
+
 
 
 resource "azurerm_subnet_network_security_group_association" "nsg_mgmt" {
